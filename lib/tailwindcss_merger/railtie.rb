1e2b1a49579d496aff8c8129cc9f0732e4c8b9e5
loqimean <vanuha277@gmail.com>
2022-02-19 14:05:53 +0200

    chore(lib/tailwindcss_merger) load tasks to rails app

---
+++ lib/tailwindcss_merger/railtie.rb
@@ -0,0 +1,11 @@
+module TailwindcssMerger
+  class Railtie < ::Rails::Railtie
+    railtie_name :tailwindcss_merger
+
+    rake_tasks do
+      path = File.expand_path(__dir__)
+
+      Dir.glob("#{path}/../tasks/**/*.rake").each { |f| load f }
+    end
+  end
+end
